e17366f42baa5d70e6b915f9130a3185ff5f87fd
Matias Fernandez <matifer.dev@gmail.com>
2024-02-24 01:25:27 -0300

    Comenzando con el filtrado por nombre

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,8 +2,15 @@ import { useState } from 'react';
 import { Person } from './components/Person';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterByName, setFilterByName] = useState();
 
   const addName = (event) => {
     event.preventDefault();
@@ -25,14 +32,25 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
   // const personsToShow =
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
